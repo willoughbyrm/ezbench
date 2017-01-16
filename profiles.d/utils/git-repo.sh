df8ad78325dad6bd7e09cad6b2eed9997a035f71
Martin Peres <martin.peres@free.fr>
2017-01-17 01:14:17 +0200

    core/git-repo: do not show warning of the deploy path does not exist

--- profiles.d/utils/git-repo.sh
+++ profiles.d/utils/git-repo.sh
@@ -125,7 +125,7 @@ function profile_get_version_list() {
 # Outputs:
 #   - Space-separated list of versions
 function profile_get_built_versions() {
-    pushd  $PROFILE_DEPLOY_BASE_DIR > /dev/null || return 1
+    pushd  "$PROFILE_DEPLOY_BASE_DIR" 2>&1 > /dev/null || return 1
     for file in *; do
         echo -n "$file "
     done
