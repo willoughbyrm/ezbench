willoughbyrm/ezbench

commit 814cdf18e4ee575fa0ed3c2e27a555fdc6e2d95e

smartezbench: do print messages in force_test_rounds()

python-modules/ezbench/smartezbench.py
@@ -553,12 +553,7 @@ class SmartEzbench:
         to_add = at_least - self.state['commits'][commit]['tests'][test]['rounds']
 
         if to_add > 0:
-            self.__log(Criticality.WW,
-                       "Schedule {} more runs for the test {} on commit {}".format(to_add, test, commit))
-
             self.state['commits'][commit]['tests'][test]['rounds'] += to_add
-
-        if to_add > 0:
             return to_add
         else:
             return 0
@@ -1097,20 +1092,24 @@ class SmartEzbench:
         # biggest score to speed up bisecting of the most important issues
         scheduled_commits = added = 0
         self.__reload_state(keep_lock=True)
-        added = 0
+        total_added = 0
         while len(tasks_sorted) > 0 and scheduled_commits < commit_schedule_max:
             commit = tasks_sorted[-1][1]
             self.__log(Criticality.DD, "Add all the tasks using commit {}".format(commit))
             for t in tasks_sorted:
                 if t[1] == commit:
-                    added += self.__force_test_rounds_unlocked__(t[1], t[2], t[3])
-            if added > 0:
+                    added = self.__force_test_rounds_unlocked__(t[1], t[2], t[3])
+                    if added > 0:
+                        self.__log(Criticality.II,
+                                "Scheduled {} more runs for the test {} on commit {}".format(added, test, commit))
+                    total_added += added
+            if total_added > 0:
                 self.__log(Criticality.II, "{}".format(t[4]))
                 scheduled_commits += 1
             else:
                 self.__log(Criticality.DD, "No work scheduled using commit {}, try another one".format(commit))
             del tasks_sorted[-1]
-        if added > 0:
+        if total_added > 0:
             self.__save_state()
         self.__release_lock()
 

commit 58bb55e18561be804f25513af16cf2656e17947c

smartezbench: fix the condition that sets the running state to DONE
Fixes: 814cdf18e4ee575fa0ed3c2e27a555fdc6e2d95e

python-modules/ezbench/smartezbench.py
@@ -1092,9 +1092,8 @@ class SmartEzbench:
 
         # If we are using the throttle mode, only schedule the commit with the
         # biggest score to speed up bisecting of the most important issues
-        scheduled_commits = added = 0
+        scheduled_commits = total_added = 0
         self.__reload_state(keep_lock=True)
-        total_added = 0
         while len(tasks_sorted) > 0 and scheduled_commits < commit_schedule_max:
             commit = tasks_sorted[-1][1]
             self.__log(Criticality.DD, "Add all the tasks using commit {}".format(commit))
@@ -1117,8 +1116,7 @@ class SmartEzbench:
 
         self.__log(Criticality.II, "Done enhancing the report")
 
-
-        if (added == 0 and
+        if (total_added == 0 and
             self.__running_mode_unlocked__(check_running=False) == RunningMode.RUN):
             self.set_running_mode(RunningMode.DONE)
 