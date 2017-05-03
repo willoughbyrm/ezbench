willoughbyrm/ezbench

commit c14e87473b4cbf761089e5a55a538963f47ccdcd

ezbench: improve the add_test output

python-modules/ezbench/smartezbench.py
@@ -485,13 +485,18 @@ class SmartEzbench:
         else:
             task_tree[commit]['tests'][test]['rounds'] += rounds
 
+        total_rounds = task_tree[commit]['tests'][test]['rounds']
+
         # if the number of rounds is equal to 0 for a test, delete it
         if task_tree[commit]['tests'][test]['rounds'] <= 0:
             del task_tree[commit]['tests'][test]
+            total_rounds = 0
 
         # Delete a commit that has no test
         if len(task_tree[commit]['tests']) == 0:
             del task_tree[commit]
+
+        return total_rounds
 
     def __add_test_unlocked__(self, commit, test, rounds):
         scm = self.repo()
@@ -499,9 +504,9 @@ class SmartEzbench:
             commit = scm.full_version_name(commit)
 
         if rounds is None:
-            return
+            return self.state['commits'][commit]['tests'][test]['rounds']
 
-        self.__task_tree_add_test__(self.state['commits'], commit, test, rounds)
+        return self.__task_tree_add_test__(self.state['commits'], commit, test, rounds)
 
         # If the state was DONE, set it back to RUN
         if self.__running_mode_unlocked__(check_running=False) == RunningMode.DONE:
@@ -509,9 +514,10 @@ class SmartEzbench:
 
     def add_test(self, commit, test, rounds = None):
         self.__reload_state(keep_lock=True)
-        self.__add_test_unlocked__(commit, test, rounds)
+        total_rounds = self.__add_test_unlocked__(commit, test, rounds)
         self.__save_state()
         self.__release_lock()
+        return total_rounds
 
     def add_testset(self, commit, testset, rounds = 1):
         self.__reload_state(keep_lock=True)

ezbench
@@ -183,8 +183,15 @@ if args.commits is not None and args.tests is not None:
     # Add all the commits and tests to commit
     for commit in run_info.commits:
         for test in run_info.tests:
-            print("add {count} runs to {test} on {commit}".format(count=args.rounds, test=test, commit=commit))
-            sbench.add_test(commit, test, rounds)
+            total_rounds = sbench.add_test(commit, test, args.rounds)
+            if args.rounds >= 0:
+                print("added {} runs to {} on {} --> {} runs".format(rounds,
+                                                                     test, commit,
+                                                                     total_rounds))
+            else:
+                print("removed {} runs to {} on {} --> {} runs".format(-args.rounds,
+                                                                       test, commit,
+                                                                       total_rounds))
 
 if args.commits is not None and len(testsets_to_be_added) > 0:
     # remove duplicates in the lists

commit f47aecb220f687560d87dc7133ff23565f1f8ba6

smartezbench: when adding rounds, reset the status to RUN when it was DONE
Fixes: c14e87473b4cbf761089e5a55a538963f47ccdcd

python-modules/ezbench/smartezbench.py
@@ -505,14 +505,18 @@ class SmartEzbench:
         if scm is not None:
             commit = scm.full_version_name(commit)
 
+        total_rounds = self.state['commits'][commit]['tests'][test]['rounds']
         if rounds is None:
             return self.state['commits'][commit]['tests'][test]['rounds']
 
-        return self.__task_tree_add_test__(self.state['commits'], commit, test, rounds)
+        new_total_rounds = self.__task_tree_add_test__(self.state['commits'], commit, test, rounds)
 
-        # If the state was DONE, set it back to RUN
-        if self.__running_mode_unlocked__(check_running=False) == RunningMode.DONE:
+        # If we added rounds and the state was DONE, set it back to RUN
+        if (new_total_rounds > total_rounds and
+            self.__running_mode_unlocked__(check_running=False) == RunningMode.DONE):
             self.__set_running_mode_unlocked__(RunningMode.RUN)
+
+        return new_total_rounds
 
     def add_test(self, commit, test, rounds = None):
         self.__reload_state(keep_lock=True)